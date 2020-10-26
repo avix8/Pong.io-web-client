5598d6eb379437aa546c524025c38ce33d881418
AVIX <avix256@gmail.com>
2020-10-26 23:58:16 +0700

    убрал конфликтое правило и изменил название

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,8 +24,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - lolar-web-client',
-    title: 'lolar-web-client',
+    titleTemplate: '%s - Pong.io',
+    title: 'Pong.io',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
