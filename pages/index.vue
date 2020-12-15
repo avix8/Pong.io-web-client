eb0898988992e949fd1a7ec9cbde4a7007759214
AVIX <avix256@gmail.com>
2020-12-16 00:34:17 +0700

    изменил описание на главной странице

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,9 @@
         <v-card width="400">
           <v-card-title class="justify-center text-h3"> Pong.io </v-card-title>
           <v-card-text>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora
-            corporis cupiditate illum a consequuntur dolore provident nobis
-            perspiciatis? Reprehenderit, quasi laudantium tenetur atque possimus
-            asperiores nobis illo nostrum dolore voluptatem.
+            Многопользовательская игра, в которой игроки располагаются по кругу
+            и отбивают мячи. Побеждает игрок набравший 100 очков. Колчество
+            игроков не менее трёх.
           </v-card-text>
           <v-card-actions>
             <v-btn block @click="createRoom">Создать комнату</v-btn>
