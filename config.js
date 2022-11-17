388d270b0ce8bcd5931d021795b666240c90ff43
AVIX <avix256@gmail.com>
2022-11-17 20:08:15 +0700

    new backendURL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const config = {
-  backendURL: 'https://pong-io-node-js-server.vercel.app:4000', // 'http://localhost:4000'
+  backendURL: 'http://154.12.236.123:4000', // 'http://localhost:4000'
   // frontendURL: 'https://pong-io-web-client.vercel.app:3000', // 'http://localhost:3000'
   frontendURL: 'http://localhost:3000', // 'http://localhost:3000'
 }
